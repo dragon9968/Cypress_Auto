5f5bc75a5f7d7bc20241268a7af2ac5a9efff7a3
Leon Nguyen <leon52383@gmail.com>
2022-06-24 11:05:39 -0400

    Clean set up for initial angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes =
+  [
+    { path: 'notfound', component: NotfoundComponent },
+    { path: '', component: LandingComponent },
+    { path: '**', redirectTo: 'notfound', pathMatch: 'full' }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BackButtonDisableModule } from 'angular-disable-browser-back-button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BackButtonDisableModule.forRoot({ preserveScrollPosition: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
