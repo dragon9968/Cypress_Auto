ef8c671d52edc18f6785a310db7b94d22f82614c
Duc Nguyen <duc.nguyen@evizi.com>
2022-06-30 14:50:25 +0700

    KR-1563 Import Cytoscape JS library to Angular

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { BaseDialogComponent } from './dialogs/base/base-dialog.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
@@ -16,10 +14,6 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory,
-    }),
   ],
   exports: [
     CommonModule,
