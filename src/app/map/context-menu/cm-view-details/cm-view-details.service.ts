5973193650f4ddc6f0a2fe41caeba1d56754ba08
Duc Nguyen <duc.nguyen@evizi.com>
2022-08-23 16:39:36 +0700

    KR-1681 Associate event for Interface View

--- src/app/map/context-menu/cm-view-details/cm-view-details.service.ts
+++ src/app/map/context-menu/cm-view-details/cm-view-details.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { InterfaceService } from 'src/app/core/services/interface/interface.service';
 import { NodeService } from 'src/app/core/services/node/node.service';
 import { PortGroupService } from 'src/app/core/services/portgroup/portgroup.service';
 import { AddUpdateNodeDialogComponent } from 'src/app/map/add-update-node-dialog/add-update-node-dialog.component';
+import { AddUpdateInterfaceDialogComponent } from '../../add-update-interface-dialog/add-update-interface-dialog.component';
 import { AddUpdatePGDialogComponent } from '../../add-update-pg-dialog/add-update-pg-dialog.component';
 
 @Injectable({
@@ -14,7 +16,8 @@ export class CMViewDetailsService {
     private dialog: MatDialog,
     private nodeService: NodeService,
     private portGroupService: PortGroupService,
-  ) {}
+    private interfaceService: InterfaceService,
+  ) { }
 
   getMenu() {
     return {
@@ -24,7 +27,13 @@ export class CMViewDetailsService {
       onClickFunction: (event: any) => {
         let data = event.target.data()
         if (data.interface_id) {
-          // this.map_forms.openInterfaceInfo(data.interface_id);
+          this.interfaceService.get(data.interface_id).subscribe(interfaceData => {
+            const dialogData = {
+              mode: 'view',
+              genData: interfaceData.result,
+            }
+            this.dialog.open(AddUpdateInterfaceDialogComponent, { width: '600px', data: dialogData });
+          });
         } else if (data.pg_id) {
           this.portGroupService.get(data.pg_id).subscribe(pgData => {
             const dialogData = {
