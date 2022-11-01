db85f4520b4c9d6904df68b2f54ef090145d6287
Yao Jiang <yjiang@blue-cloak.com>
2022-11-01 13:12:23 -0400

    Fix docker deploy script.

--- nightly-build.sh
+++ nightly-build.sh
@@ -1,6 +1,8 @@
-#/bin/bash
+#!/bin/bash
 appdir=/opt/keromatsu
 composefile=$appdir/docker-compose-frontend.yml
+jenkinsArchive=$appdir/jenkins/frontend
+mkdir -p $jenkinsArchive
 
 #default build type
 targetCompose="docker-compose-prod.yml"
@@ -20,13 +22,27 @@ echo
 ./build-image.sh
 ./docker-cleanup.sh
 
-echo "restart containers"
-echo
 #copy docker-compose file to target if doesn't exist
 [ ! -f $composefile ] && cp ./$targetCompose $composefile
+destFile="$appdir/docker-compose-frontend.yml"
+destBackup="$jenkinsArchive/docker-compose-frontend.yml.bak"
+if [ ! -f $composefile ]; then 
+    echo "docker-compose file does not exist, copying new file to $destFile."
+    cp ./$targetCompose $composefile
+else
+    #compare the two files to see if they are the same
+    if cmp -s "$destFile" "./$targetCompose"; then
+        echo "docker-compose file is same, skip update."
+    else
+        echo "new docker-compose file detected, backing up old docker-compose.yml"
+        cp "$destFile" "$destBackup"
+        echo "updating target compose file"
+        cp "./$targetCompose" "$destFile"
+    fi
+fi
 #archive the build artifacts to jenkins directory for reference later
-jenkinsArchive=$appdir/jenkins/frontend
-mkdir -p $jenkinsArchive
+echo "restart containers"
+echo
 cp ./$targetCompose $jenkinsArchive
 cp make-dist.sh $jenkinsArchive
 
