aeff3de73bb3a40c7f12a95b7b2452b449cd8f28
Duc Nguyen Dinh <duc.nguyen@evizi.com>
2022-11-18 16:01:55 +0000

    Merged in bugfix/KR-1962 (pull request #160)
    KR-1962 [Configuration Templates] The error messages of required fields should be displayed when a user clicks on [Save] button without entering value into required fields
    
    * KR-1962 [Configuration Templates] The error messages of required fields should be displayed when a user clicks on [Save] button without entering value into required fields
    
    
    Approved-by: Kenny Nguyen

--- src/app/templates/config-templates/add-route-dialog/add-route-dialog.component.ts
+++ src/app/templates/config-templates/add-route-dialog/add-route-dialog.component.ts
@@ -1,9 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { ToastrService } from 'ngx-toastr';
 import { ConfigTemplateService } from 'src/app/core/services/config-template/config-template.service';
+import { HelpersService } from 'src/app/core/services/helpers/helpers.service';
+import { ErrorMessages } from 'src/app/shared/enums/error-messages.enum';
 import { retrievedConfigTemplates } from 'src/app/store/config-template/config-template.actions';
 
 @Component({
@@ -14,24 +16,27 @@ import { retrievedConfigTemplates } from 'src/app/store/config-template/config-t
 export class AddRouteDialogComponent implements OnInit {
   configForm!: FormGroup;
   isViewMode = false;
+  errorMessages = ErrorMessages;
+
   constructor(
     private configTemplateService: ConfigTemplateService,
     private toastr: ToastrService,
     private store: Store,
+    public helpers: HelpersService,
     public dialogRef: MatDialogRef<AddRouteDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-  ) { 
+  ) {
     this.isViewMode = this.data.mode == 'view';
     this.configForm = new FormGroup({
-      route: new FormControl({value: '', disabled: this.isViewMode}),
-      nextHop: new FormControl({value: '', disabled: this.isViewMode}),
-      interface: new FormControl({value: '', disabled: this.isViewMode})
+      routeCtr: new FormControl({ value: '', disabled: this.isViewMode }, [Validators.required]),
+      nextHopCtr: new FormControl({ value: '', disabled: this.isViewMode }, [Validators.required]),
+      interfaceCtr: new FormControl({ value: '', disabled: this.isViewMode })
     });
   }
 
-  get route() {return this.configForm.get('route');}
-  get nextHop() {return this.configForm.get('nextHop');}
-  get interface() {return this.configForm.get('interface');}
+  get routeCtr() {return this.configForm.get('routeCtr');}
+  get nextHopCtr() {return this.configForm.get('nextHopCtr');}
+  get interfaceCtr() {return this.configForm.get('interfaceCtr');}
 
   ngOnInit(): void {
   }
@@ -39,20 +44,20 @@ export class AddRouteDialogComponent implements OnInit {
   addRoute() {
     const jsonData = {
       config_type: "static_route",
-      config_id: this.data.genData.id, 
+      config_id: this.data.genData.id,
       name: this.data.genData.name,
       description: this.data.genData.description,
-      route: this.route?.value,
-      next_hop: this.nextHop?.value,
-      interface: this.interface?.value,
+      route: this.routeCtr?.value,
+      next_hop: this.nextHopCtr?.value,
+      interface: this.interfaceCtr?.value,
     }
     this.configTemplateService.addConfiguration(jsonData).subscribe({
-      next: (rest) =>{
+      next: (rest) => {
         this.toastr.success(`Add Route successfully`);
         this.dialogRef.close();
-        this.configTemplateService.getAll().subscribe((data: any) => this.store.dispatch(retrievedConfigTemplates({data: data.result})));
+        this.configTemplateService.getAll().subscribe((data: any) => this.store.dispatch(retrievedConfigTemplates({ data: data.result })));
       },
-      error:(err) => {
+      error: (err) => {
         this.toastr.error(`Error while add Route`);
       }
     });
@@ -61,5 +66,4 @@ export class AddRouteDialogComponent implements OnInit {
   onCancel() {
     this.dialogRef.close();
   }
-
 }
