e76825c269447cd278f4a95d24f3bb6f48838c43
Trung Nguyen <sangtrung.nguyen@evizi.com>
2023-01-19 16:05:21 +0000

    Merged in feature/KR-2269 (pull request #332)
    KR-2269 Project export populate with default file name with project name, time, date
    
    * KR-2269 Project export populate with default file name with project name, time, date
    
    * Updated export name.
    
    
    Approved-by: Kenny Nguyen

--- src/app/core/services/helpers/helpers.service.ts
+++ src/app/core/services/helpers/helpers.service.ts
@@ -681,6 +681,17 @@ export class HelpersService implements OnDestroy{
     return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
+  getDateformatYYYYMMDDHHMMSS(date: Date)
+  {
+    const year = '' + date.getFullYear();
+    let month = '' + (date.getMonth() + 1); if (month.length == 1) { month = '0' + month; }
+    let day = '' + date.getDate(); if (day.length == 1) { day = '0' + day; }
+    let hour = '' + date.getHours(); if (hour.length == 1) { hour = '0' + hour; }
+    let minute = '' + date.getMinutes(); if (minute.length == 1) { minute = '0' + minute; }
+    let second = '' + date.getSeconds(); if (second.length == 1) { second = '0' + second; }
+    return year + month + day + '-' + hour + minute + second;
+  }
+
   validateAllFormFields(formGroup: FormGroup) {
     Object.keys(formGroup.controls).forEach(field => {
       const control = formGroup.get(field);
--- src/app/project/export-project-dialog/export-project-dialog.component.ts
+++ src/app/project/export-project-dialog/export-project-dialog.component.ts
@@ -1,34 +1,48 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { HelpersService } from 'src/app/core/services/helpers/helpers.service';
 import { ErrorMessages } from 'src/app/shared/enums/error-messages.enum';
 import { ProjectService } from '../services/project.service';
+import { Subscription } from "rxjs";
+import { Store } from "@ngrx/store";
+import { selectProjectName } from "../../store/project/project.selectors";
 
 @Component({
   selector: 'app-export-project-dialog',
   templateUrl: './export-project-dialog.component.html',
   styleUrls: ['./export-project-dialog.component.scss']
 })
-export class ExportProjectDialogComponent implements OnInit {
+export class ExportProjectDialogComponent implements OnInit, OnDestroy {
   exportForm!: FormGroup;
   errorMessages = ErrorMessages;
+  selectProjectName$ = new Subscription();
+  projectName = '';
   constructor(
+    private store: Store,
     private projectService: ProjectService,
     private toastr: ToastrService,
     private helpers: HelpersService,
     public dialogRef: MatDialogRef<ExportProjectDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-  ) { 
+  ) {
     this.exportForm = new FormGroup({
       fileNameCtr: new FormControl('', [Validators.required])
     })
+    this.selectProjectName$ = this.store.select(selectProjectName).subscribe(projectName => {
+      this.projectName = projectName
+    })
   }
 
-  get fileNameCtr() {return this.exportForm.get('fileNameCtr')}
+  ngOnDestroy(): void {
+    this.selectProjectName$.unsubscribe();
+  }
+
+  get fileNameCtr() { return this.exportForm.get('fileNameCtr') }
 
   ngOnInit(): void {
+    this.fileNameCtr?.setValue(this.projectName + '-' + this.helpers.getDateformatYYYYMMDDHHMMSS(new Date()));
   }
 
   onCancel() {
@@ -43,7 +57,7 @@ export class ExportProjectDialogComponent implements OnInit {
     let file = new Blob();
     var fileName = this.fileNameCtr?.value
     fileName = fileName.replace(/\s+/g, "_");
-    const fullFileName = `${fileName}-Export.json` 
+    const fullFileName = `${fileName}.json`
     this.projectService.exportProject(jsonData).subscribe(response => {
       file = new Blob([JSON.stringify(response, null, 4)], {type: 'application/json'});
       this.helpers.downloadBlob(fullFileName, file);
