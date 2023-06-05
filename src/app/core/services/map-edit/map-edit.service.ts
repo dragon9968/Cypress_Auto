d645fa2bb8f2562e8a2bf155ce34a24202a5ceb8
Trung Nguyen <sangtrung.nguyen@evizi.com>
2023-06-05 00:49:23 +0000

    Merged in bugfix/KR-2665-re-work (pull request #558)
    KR-2665 [Link Project] The system automatically displays Direction as Both  when adding a link Project on the map
    
    * KR-2665 [Link Project] The system automatically displays Direction as Both  when adding a link Project on the map
    
    
    Approved-by: Kenny Nguyen

--- src/app/core/services/map-edit/map-edit.service.ts
+++ src/app/core/services/map-edit/map-edit.service.ts
@@ -50,9 +50,43 @@ export class MapEditService implements OnDestroy{
     this.selectMapProperties$.unsubscribe();
   }
 
+  convertNodeElementOnMapToDataFormat(nodeEle: any) {
+    return { data: nodeEle.data(), layout: { name: 'present'}, position: nodeEle.position() }
+  }
+
+  convertEdgeElementOnMapToDataFormat(edgeEle: any) {
+    return { data: edgeEle.data()}
+  }
+
+  isNodeNotInProjectNode(nodeElem: any) {
+    return nodeElem.data('elem_category') != 'map_link' && !(nodeElem.data('parent') && nodeElem.data('parent').includes('project-link'))
+  }
+
   updateGroupBoxesInMapStorage(cy: any, groupBoxes: any[]) {
-    const nodesOnMap = cy.nodes().map((ele: any) => ({ data: ele.data(), layout: { name: 'present'}, position: ele.position() }))
-    const edgesOnMap = cy.edges().map((ele: any) => ({ data: ele.data()}));
+    let nodesInProjectNodes: any = []
+    let edgesInProjectNodes: any = []
+    const nodesEleNotInProjectNode = cy.nodes().filter(this.isNodeNotInProjectNode)
+    let nodesOnMap = nodesEleNotInProjectNode.map(this.convertNodeElementOnMapToDataFormat)
+    const nodeIdsInProjectNode = nodesOnMap.filter((ele: any) => ele.data.node_id).map((e: any) => e.data.node_id)
+    const pgIdsInProjectNode = nodesOnMap.filter((ele: any) => ele.data.pg_id).map((e: any) => e.data.pg_id)
+    const edgesEleNotInProjectNode = cy.edges((edge: any) =>
+      !(edge.data('node_id') in nodeIdsInProjectNode || edge.data('pg_id') in pgIdsInProjectNode)
+    )
+    let edgesOnMap = edgesEleNotInProjectNode.map(this.convertEdgeElementOnMapToDataFormat);
+    cy.nodes('[elem_category="map_link"]').map((ele: any) => {
+      const children = ele.data('collapsedChildren') || ele.children()
+      if (children && children.length > 0) {
+        nodesInProjectNodes = children.nodes().map(this.convertNodeElementOnMapToDataFormat);
+        nodesOnMap = nodesOnMap.concat(nodesInProjectNodes);
+        edgesInProjectNodes = children.edges().map(this.convertEdgeElementOnMapToDataFormat);
+        edgesOnMap = edgesOnMap.concat(edgesInProjectNodes);
+        // Remove collapsedChildren
+        const projectNodeData = ele.data();
+        delete projectNodeData.collapsedChildren;
+        nodesOnMap.push({ data: projectNodeData, layout: { name: 'present'}, position: ele.position() })
+      }
+    })
+
     const newMapProperties = JSON.parse(JSON.stringify(this.mapProperties))
     newMapProperties.default_preferences.map_style.edge_direction_checkbox = this.isEdgeDirectionChecked
     newMapProperties.default_preferences.map_style.groupbox_checkbox = this.isGroupBoxesChecked
@@ -74,7 +108,7 @@ export class MapEditService implements OnDestroy{
   }
 
   removeAllProjectNodesOnMap(cy: any) {
-    const projectNodes = cy.nodes().filter((node: any) => node.data('elem_category') == 'map_link' && !Boolean(node.data('parent_id')))
+    const projectNodes = cy?.nodes().filter((node: any) => node.data('elem_category') == 'map_link' && !Boolean(node.data('parent_id')))
     if (projectNodes && projectNodes.length > 0) {
       projectNodes.map((projectNode: any) => {
         const data = projectNode.data();
