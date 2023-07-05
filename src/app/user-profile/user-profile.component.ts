a67cc35a3f11455507493da2404ae3ed2ded4147
Duc Nguyen Dinh <duc.nguyen@evizi.com>
2023-07-05 01:36:47 +0000

    Merged in bugfix/KR-2908 (pull request #624)
    KR-2908 [User Profile] Should display an error message when user updates user profile with an email invalid
    
    * KR-2908 [User Profile] Should display an error message when user updates user profile with an email invalid
    
    
    Approved-by: Yao Jiang
    Approved-by: Kenny Nguyen

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Subscription, catchError, throwError } from 'rxjs';
 import { ErrorMessages } from '../shared/enums/error-messages.enum';
@@ -32,7 +32,7 @@ export class UserProfileComponent implements OnInit {
       usernameCtr: new FormControl(''),
       firstNameCtr: new FormControl(''),
       lastNameCtr: new FormControl(''),
-      emailCtr: new FormControl(''),
+      emailCtr: new FormControl('', [Validators.email]),
       roleCtr: new FormControl({ value: '', disabled: true }),
     });
   }
